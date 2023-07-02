db30ad2a0bcf8da7a4e8a47d2e8db1b7192e563b
LeonarDev <leonardo.majevski@gmail.com>
2023-07-02 19:23:10 -0300

    feat02 - Implements Dependency Injection

--- SpeedEat.Api/Program.cs
+++ SpeedEat.Api/Program.cs
@@ -1,23 +1,18 @@
-using SpeedEat.Application.Services.Authentication;
+using SpeedEat.Application;
+using SpeedEat.Infra;
 
 var builder = WebApplication.CreateBuilder(args);
 {
-    builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+    builder.Services
+        .AddApplication()
+        .AddInfrastructure();
+    
     builder.Services.AddControllers();
-    // builder.Services.AddEndpointsApiExplorer();
-    // builder.Services.AddSwaggerGen();
 }
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
-
-app.UseHttpsRedirection();
-// app.UseAuthorization();
-app.MapControllers();
-app.Run();
+{
+    app.UseHttpsRedirection();
+    app.MapControllers();
+    app.Run();
+}
